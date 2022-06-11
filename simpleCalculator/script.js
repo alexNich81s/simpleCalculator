97c88cfd36c513e969864195a1762fc92414f057
alexNich81s <78985355+alexNich81s@users.noreply.github.com>
2022-06-11 15:50:06 -0500

    Add files via upload

---
+++ simpleCalculator/script.js
@@ -0,0 +1,20 @@
+document.getElementById('output').readOnly = true;
+let box = document.getElementById('output');
+buttons = document.querySelectorAll('button');
+let calculatorValue = "";
+for(num of buttons){
+  num.addEventListener('click', (e) => {
+    buttonText = e.target.innerText;
+    if(buttonText == "="){
+      box.value = eval(calculatorValue);
+    }
+    else if(buttonText == "Clear"){
+      calculatorValue = '';
+      box.value = calculatorValue;
+    }
+    else{
+      calculatorValue += buttonText;
+      box.value = calculatorValue;
+    }
+  })
+}
